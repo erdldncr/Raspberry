db7bde9f23defabbf37e1330da496d73ced0b7e2
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-01-31 04:00:47 +0000

    date added responsive header has been done

--- HomePage/javascript.js
+++ HomePage/javascript.js
@@ -12,4 +12,8 @@ for(var i=0;i<a.length;i++){
     document.querySelectorAll("h4")[i].style.backgroundColor="#"+randomBgColor;
    
 }
+var days=["SUNDAY",'MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY']
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var date=new Date();
+document.querySelector(".date p").innerHTML=date.getUTCFullYear()+"-"+months[date.getUTCMonth()]+"-"+date.getUTCDate()+"-"+days[date.getUTCDay()]
 
