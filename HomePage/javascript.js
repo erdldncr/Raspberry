8f8dfcfdc1811094445b9114c8c149ce239d193f
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-02-01 04:21:26 +0000

    Media queries has been added and some corrections has been done

--- HomePage/javascript.js
+++ HomePage/javascript.js
@@ -4,7 +4,7 @@ document.querySelector("select").addEventListener('click',function(){
 })
 
 
-var a=document.querySelectorAll("h4");
+var a=document.querySelectorAll(".get-started h4");
 console.log(a)
 for(var i=0;i<a.length;i++){
     var randomBgColor=Math.floor(Math.random()*16777215).toString(16);
@@ -17,3 +17,31 @@ var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'A
 var date=new Date();
 document.querySelector(".date p").innerHTML=date.getUTCFullYear()+"-"+months[date.getUTCMonth()]+"-"+date.getUTCDate()+"-"+days[date.getUTCDay()]
 
+// GET-STRATED-SECTION
+var shortText=document.getElementsByClassName("short-text");
+const mediaQuery = window.matchMedia('(max-width: 770px)')
+shortText.addListener(ChageDisplay)
+
+function ChangeDisplay(mediaQuery){
+    if(!mediaQuery.matches){
+        for(var i=0;i<shortText.length;i++){
+            shortText[i].style.display="none"
+        }
+    }
+}
+// media query event handler
+// if (matchMedia) {
+//     const mq = window.matchMedia("(max-width: 770px)");
+//     mq.addListener(WidthChange);
+//     WidthChange(mq);
+//     }
+    
+//     // media query change
+//     function WidthChange(mq) {
+//     if (mq.matches) {
+//     // window width is at least 500px
+//     } else {
+//     // window width is less than 500px
+//     }
+    
+//     }
